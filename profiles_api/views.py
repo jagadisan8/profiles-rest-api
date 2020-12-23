0e11400dcd297ccdc48798c548ce48beeb3a1bdf
jagadisan <sjagadish717@gmail.com>
2020-12-24 00:09:22 +0530

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    def get(self,request,format=None):
+        Friends=[
+                "elango",
+                "vishal",
+                "lokesh"
+                ]
+        return Response({"welcome":"hello!","Friends":Friends})
