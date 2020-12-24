7dba48db701d7aa1a81089ad609cd8d36fe5a291
jagadisan <sjagadish717@gmail.com>
2020-12-25 01:16:45 +0530

    Added viewset to profile_api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 from profiles_api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset',views.HelloViewset,base_name='hello-viewset')
 
 urlpatterns = [
-    path('myfirst-apiview/',views.HelloApiView.as_view())
+    path('myfirst-apiview/',views.HelloApiView.as_view()),
+    path('',include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework import viewsets
 
 class HelloApiView(APIView):
     serializer_class = serializers.HelloSerializer
@@ -32,3 +33,38 @@ class HelloApiView(APIView):
 
     def delete(self,request,pk=None):
         return Response({'method':'delete'})
+
+class HelloViewset(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+    def list(self,request):
+        places = [
+                "USA",
+                "INDIA",
+                "CHINA",
+                "RUSSIA"
+                ]
+        return Response({"message":"Hello","places":places})
+
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello! {name}'
+            return Response({"message":message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self,request,pk=None):
+        return Response({"HTTP_METHOD":"GET"})
+
+    def update(self,request,pk=None):
+        return Response({"HTTP_METHOD":"PUT"})
+
+    def partial_update(self,request,pk=None):
+        return Response({"HTTP_METHOD":"PATCH"})
+
+    def destroy(self,request,pk=None):
+        return Response({"HTTP_METHOD":"DELETE"})
